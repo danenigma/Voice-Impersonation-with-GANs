69ae5a3f4bd42712cae9cbe7d911ddda0d17ce48
danenigma <danielfekaduenigma@yahoo.com>
2018-10-28 23:17:00 -0400

    preprocessing done

---
+++ utils/preprocess_data.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.io import wavfile
+from pydub import AudioSegment
+import tempfile
+import os
+
+def read_spect_matrix(audio_file_list):
+	
+	data = []
+	
+	for file in audio_file_list:
+		
+		chunk = AudioSegment.from_file(file, "mp4")
+		new_file, filename = tempfile.mkstemp()
+		chunk.export(filename, format="wav")
+		
+		sample_rate, samples = wavfile.read(filename)
+		frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
+		print(spectrogram.shape)
+		data.append(spectrogram)
+		os.close(new_file)
+		
+	return data
+	
+if __name__ == '__main__':
+
+	audio_file_list = ['../data/00001.m4a']*10	
+	data = read_spect_matrix(audio_file_list)
+	
+	'''
+	plt.imshow(data[0])
+	plt.ylabel('Frequency [Hz]')
+	plt.xlabel('Time [sec]')
+	plt.show()
+	'''
